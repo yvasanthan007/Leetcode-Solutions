0c150c62685f60a10580746f14f66b25f658a105
Vasanthan Yogeshwaran <yvasanthan007@gmail.com>
2026-06-21 11:38:31 +0530

    Add Single Number
    Updated comments for clarity and formatting.

--- Arrays/136. Single_Number.py
+++ Arrays/136. Single_Number.py
@@ -1,19 +1,19 @@
 # Intuition
-'''Use XOR operation. A number XOR itself becomes 0, so all duplicate numbers cancel out and only the single number remains.'''
+Use XOR operation. A number XOR itself becomes 0, so all duplicate numbers cancel out and only the single number remains.
 
 # Approach
-''' 1. Initialize a variable `r` as 0.
-    2. Traverse the array.
-    3. XOR each element with `r`.
-    4. After all operations, `r` will contain the single number.
-    5. Return `r`.'''
+1. Initialize a variable `r` as 0.
+2. Traverse the array.
+3. XOR each element with `r`.
+4. After all operations, `r` will contain the single number.
+5. Return `r`.
 
 # Complexity
-''' - Time complexity:
-        O(n)
+- Time complexity:
+    O(n)
 
-    - Space complexity:
-        O(1)'''
+- Space complexity:
+    O(1)
 
 # Code
 class Solution:
