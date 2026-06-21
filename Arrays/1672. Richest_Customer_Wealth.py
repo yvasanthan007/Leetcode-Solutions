ba67096607a52058c2bb807fe84393dffeec027d
Vasanthan Yogeshwaran <yvasanthan007@gmail.com>
2026-06-21 11:40:42 +0530

    Add Richest Customer Wealth
    Updated comments for clarity and formatting.

--- Arrays/1672. Richest_Customer_Wealth.py
+++ Arrays/1672. Richest_Customer_Wealth.py
@@ -1,17 +1,17 @@
 # Intuition
-'''Find the total wealth of each customer by summing their accounts and return the maximum wealth.'''
+Find the total wealth of each customer by summing their accounts and return the maximum wealth.
 
 # Approach
-''' 1. Calculate the sum of each customer's accounts.
-    2. Find the maximum sum among all customers.
-    3. Return the maximum wealth.'''
+1. Calculate the sum of each customer's accounts.
+2. Find the maximum sum among all customers.
+3. Return the maximum wealth.
 
 # Complexity
-''' - Time complexity:
-        O(m × n)
+- Time complexity:
+    O(m × n)
 
-    - Space complexity:
-        O(1)'''
+- Space complexity:
+    O(1)
 
 # Code
 class Solution:
