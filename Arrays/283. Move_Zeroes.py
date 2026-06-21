1c5c0a39113b76fbd80aa4efe0b73bc8d9a06a0c
Vasanthan Yogeshwaran <yvasanthan007@gmail.com>
2026-06-21 11:43:29 +0530

    Add Move Zeroes
    Updated comments to remove unnecessary quotes and formatting.

--- Arrays/283. Move_Zeroes.py
+++ Arrays/283. Move_Zeroes.py
@@ -1,19 +1,19 @@
 # Intuition
-'''Move all non-zero elements to the front while keeping their order. The remaining positions will automatically contain zeros after swapping.'''
+Move all non-zero elements to the front while keeping their order. The remaining positions will automatically contain zeros after swapping.
 
 # Approach
-''' 1. Use a pointer `left` to track the position for the next non-zero element.
-    2. Traverse the array.
-    3. If the current element is non-zero, swap it with the element at `left`.
-    4. Increment `left`.
-    5. Continue until the end of the array.'''
+1. Use a pointer `left` to track the position for the next non-zero element.
+2. Traverse the array.
+3. If the current element is non-zero, swap it with the element at `left`.
+4. Increment `left`.
+5. Continue until the end of the array.
 
 # Complexity
-''' - Time complexity:
-        O(n)
+- Time complexity:
+    O(n)
 
-    - Space complexity:
-        O(1)'''
+- Space complexity:
+    O(1)
 
 # Code
 class Solution:
