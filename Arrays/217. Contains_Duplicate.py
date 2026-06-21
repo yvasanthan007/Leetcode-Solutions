ab965536d0c7eaf9cae3fd36a694da108a2a2da5
Vasanthan Yogeshwaran <yvasanthan007@gmail.com>
2026-06-21 11:42:05 +0530

    Add Contains Duplicate
    Updated comments to remove unnecessary quotes and improve readability.

--- Arrays/217. Contains_Duplicate.py
+++ Arrays/217. Contains_Duplicate.py
@@ -1,18 +1,18 @@
 # Intuition
-'''If the array contains duplicates, converting it to a set will remove them. Compare the sizes of the array and the set.'''
+If the array contains duplicates, converting it to a set will remove them. Compare the sizes of the array and the set.
 
 # Approach
-''' 1. Convert the array into a set.
-    2. Compare the length of the array and the set.
-    3. If the lengths are different, duplicates exist.
-    4. Otherwise, all elements are unique.'''
+1. Convert the array into a set.
+2. Compare the length of the array and the set.
+3. If the lengths are different, duplicates exist.
+4. Otherwise, all elements are unique.
 
 # Complexity
-''' - Time complexity:
-        O(n)
+- Time complexity:
+    O(n)
 
-    - Space complexity:
-        O(n)'''
+- Space complexity:
+    O(n)
 
 # Code
 class Solution:
